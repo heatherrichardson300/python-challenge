de3ffa51cafef77a3b51c659a99cf0cde8c15f21
Heather <heatherrichardson300@gmail.com>
2024-10-21 21:51:34 -0700

    updating script

--- pybank/main.py
+++ pybank/main.py
@@ -4,6 +4,7 @@ import os
 file_path = os.path.join('Resources', 'budget_data.csv')
 file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
 
+#intialize counters and create variables to hold information 
 total_months = 0
 total_net = 0
 greatest_increase = -float('inf')
@@ -13,6 +14,7 @@ month_decrease = ''
 month_increase = ''
 monthly_changes = []
 
+#open file
 with open(file_path, 'r') as financial_data:
     reader = csv.reader(financial_data, delimiter= ',')    
 
@@ -39,18 +41,20 @@ with open(file_path, 'r') as financial_data:
         elif monthly_change < greatest_decrease:
             month_decrease = row[0]
             greatest_decrease = monthly_change 
-
+        #capture the average
         average = (sum(monthly_changes) - monthly_changes[0]) / 85
 
+#place calculations into a variable with f string statements
 summary_data = (
     f'Total months: {total_months} months\n'
     f'Total net: ${total_net}\n'
-    f'Average Profit Change: ${round(average, 2)}\n'
+    f'Average Profit Change: ${round(average, 2)}\n' #round the percentage
     f"Greatest Increase in Profits: {month_increase} ${greatest_increase}\n"
     f"Greatest Decrease in Profits: {month_decrease} ${greatest_decrease}\n"
 )
+#print calculations to terminal 
 print(summary_data)
 
 with open(file_to_output, 'w') as newfile:
-    newfile.write('Summary Data\n')
-    newfile.write(summary_data)
+    newfile.write('Summary Data\n') #create header
+    newfile.write(summary_data) #write calculations
